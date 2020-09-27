e425d953863270a52fa433323d5690b0479c9e48
exelix <13405476+exelix11@users.noreply.github.com>
2020-09-27 10:54:31 +0200

    Minor CertLoader changes

--- CertLoader.cs
+++ CertLoader.cs
@@ -13,25 +13,21 @@ namespace yui
 		public static X509Certificate2 FromFile(string filename)
 		{
 			PemParser pem = PemParser.FromFile(filename);
-			byte[] key = pem.GetSection("PRIVATE KEY");
-			byte[] cert = pem.GetSection("CERTIFICATE");
 
 			using RSA rsa = RSA.Create();
-			rsa.ImportPkcs8PrivateKey(key, out _);
+			rsa.ImportPkcs8PrivateKey(pem.PrivateKey, out _);
 
-			using var Cert = new X509Certificate2(cert);
+			using var Cert = new X509Certificate2(pem.Certificate);
 			return new X509Certificate2(Cert.CopyWithPrivateKey(rsa).Export(X509ContentType.Pfx));
 		}
 	}
 
 	class PemParser
 	{
-		class PemSectionNotFoundException : Exception
-		{
-			public PemSectionNotFoundException(string message) : base(message) { }
-		}
+		public byte[] PrivateKey => GetSection("PRIVATE KEY");
+		public byte[] Certificate => GetSection("CERTIFICATE");
 
-		private string PemFile;
+		private readonly string PemFile;
 
 		public PemParser(string file)
 		{
@@ -41,13 +37,23 @@ namespace yui
 		public static PemParser FromFile(string path) =>
 			new PemParser(System.IO.File.ReadAllText(path));
 
-		private int SectEndOff(string s) => PemFile.IndexOf($"-----END {s}-----");
+		private int SectEndOff(string s)
+		{
+			var off = PemFile.IndexOf($"-----END {s}-----");
+			if (off < 0)
+				throw new PemSectionEndingNotFoundException(s);
+			return off;
+		}
 
 		private int SectStartOff(string s)
 		{
 			var header = $"-----BEGIN {s}-----";
 			var off = PemFile.IndexOf(header);
-			return off < 0 ? off : off + header.Length;
+
+			if (off < 0)
+				throw new PemSectionNotFoundException(s);
+
+			return off + header.Length;
 		}
 
 		public bool HasSection(string section) =>
@@ -55,14 +61,20 @@ namespace yui
 
 		public byte[] GetSection(string section)
 		{
-			if (!HasSection(section))
-				throw new PemSectionNotFoundException(section);
-
 			var begin = SectStartOff(section);
 			var end = SectEndOff(section) - begin;
 
 			return Convert.FromBase64String(PemFile.Substring(begin, end));
 		}
 
+		class PemSectionNotFoundException : Exception
+		{
+			public PemSectionNotFoundException(string message) : base(message) { }
+		}
+
+		class PemSectionEndingNotFoundException : Exception
+		{
+			public PemSectionEndingNotFoundException(string message) : base(message) { }
+		}
 	}
 }
